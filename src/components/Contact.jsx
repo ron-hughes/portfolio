5437da6f70d779d0964982bbb020172a5ef17f78
Ron Hughes <rondonsonmusic@gmail.com>
2023-02-07 11:15:26 -0500

    updated contact route

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -61,10 +61,10 @@ const Projects = () => {
           <DivP>
             <DivStyle>
 
-      {/* I am open to any new opportunities that come my way. Please contact me if you have any inquiries! <br /><br />
+      I am open to any new opportunities that come my way. Please contact me if you have any inquiries! <br /><br />
       <a href="mailto:ron@ronhughes.dev">ron@ronhughes.dev</a><br /> <br />
-      Phone: 203-293-0333 */}
-<form method= "POST" name="contact" netlify>
+      Phone: 203-293-0333
+{/* <form method= "POST" name="contact" netlify>
       <div>
         <label htmlFor="name">Name:</label>
         <input type="text" id="name" name="name"/>
@@ -78,7 +78,7 @@ const Projects = () => {
         <textarea id="message" name="message"/>
       </div>
       <button type="submit">Submit</button>
-    </form>
+    </form> */}
 
             </DivStyle>
        
